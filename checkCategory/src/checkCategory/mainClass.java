a481b8ac8468310345717e02794ba872c42320c5
nvhviet <nvhviet@ghp-fareast.vn>
2013-09-26 12:03:29 +0700

    Edit

--- checkCategory/src/checkCategory/mainClass.java
+++ checkCategory/src/checkCategory/mainClass.java
@@ -3,5 +3,6 @@ package checkCategory;
 public class mainClass {
 	public static void main(String[] agrs){
 		System.out.println("Init checking...");
+		System.out.println("Finished checking...");
 	}
 }
