8a33bd32436c2fb86083c86771b9a585fc082a57
nvhviet <nvhviet@ghp-fareast.vn>
2013-09-27 18:25:44 +0700

    test11

--- checkCategory/src/checkCategory/mainClass.java
+++ checkCategory/src/checkCategory/mainClass.java
@@ -61,7 +61,7 @@ public class mainClass {
 			//System.out.println("testCheckin");
 			//System.out.println("testCheckin1");
 			//System.out.println("testCheckin2");
-			System.out.println("testCheckin10");
+			System.out.println("testCheckin11");
 		} catch (Exception e) {
 			//e.printStackTrace();
 			// TODO: handle exception
